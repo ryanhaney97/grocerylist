eb0fafa74f895a25f9a8e8da5b66f5d7e32bb90f
yoshiquest <yoshiquest@gmail.com>
2023-02-02 12:58:23 -0600

    Changed db initialization to not use deep merge.

--- src/grocerylist/events.cljs
+++ src/grocerylist/events.cljs
@@ -1,11 +1,10 @@
 (ns grocerylist.events
   (:require
     [grocerylist.db :as db]
-    [grocerylist.util :as u]
     [grocerylist.events.util :refer [reg-event-persistent-db select-list]]
     [grocerylist.events.forms]))
 
 (reg-event-persistent-db
   ::initialize-db
   (fn [db _]
-    (u/deep-merge db/default-db db)))
+    (merge db/default-db db)))
