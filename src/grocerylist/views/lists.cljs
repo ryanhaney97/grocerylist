7e9bd529f36c766cebc2761958a2401ad30f67c8
yoshiquest <yoshiquest@gmail.com>
2023-02-02 12:55:06 -0600

    Added ability to delete lists.

--- src/grocerylist/views/lists.cljs
+++ src/grocerylist/views/lists.cljs
@@ -2,17 +2,26 @@
   (:require
     [re-frame.core :as re-frame]
     [grocerylist.events.route :as events.route]
+    [grocerylist.events.lists :as events.lists]
     [grocerylist.subs.lists :as subs.lists]
     [grocerylist.views.util :as u]))
 
+(defn list-button [list-name]
+  (u/button-factory
+    list-name
+    (fn [id]
+      (re-frame/dispatch [::events.route/to :list id]))))
+
+(def list-delete-button
+  (u/button-factory
+    "X"
+    (fn [id]
+      (re-frame/dispatch [::events.lists/confirm-delete id]))))
+
 (defn draw-list-link [_ list]
-  (let [on-click (fn [id] (re-frame/dispatch [::events.route/to :list id]))
-        on-click-factory (u/callback-factory-factory on-click)]
-    (fn [_ list]
-      [:li
-       [:button {:type "button"
-                 :on-click (on-click-factory (:id list))}
-        (:name list)]])))
+  [:li
+   [list-delete-button (:id list)]
+   [(list-button (:name list)) (:id list)]])
 
 (defn lists-list []
   (let [lists @(re-frame/subscribe [::subs.lists/name-list])]
