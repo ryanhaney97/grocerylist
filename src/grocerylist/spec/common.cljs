1af181de6b908a9397c5ab6cffdfa7afe7e6ce0e
yoshiquest <yoshiquest@gmail.com>
2023-02-09 11:29:03 -0600

    Added a couple comments explaining the specs.

--- src/grocerylist/spec/common.cljs
+++ src/grocerylist/spec/common.cljs
@@ -3,6 +3,8 @@
     [cljs.spec.alpha :as s]
     [clojure.string :as string]))
 
+;A number of these are technically redundant (e.g. checking for nil before checking the type),
+;but are present so that the exact error can be tracked more precisely.
 (s/def ::not-nil some?)
 (s/def ::is-string (s/and ::not-nil string?))
 (s/def ::is-int (s/and ::not-nil int?))
--- src/grocerylist/spec/list.cljs
+++ src/grocerylist/spec/list.cljs
@@ -9,6 +9,13 @@
 (s/def ::location ::common/not-blank-string)
 (s/def ::locations (s/coll-of ::location :distinct true))
 (s/def ::items.next-id ::common/is-int)
+
+;Clojure Spec doesn't support cross-validating keys very well, and so the following is a workaround,
+;in order to ensure each item has a location in the list of locations. This COULD be done with a single predicate
+;checking from ::list, but if that is done it won't preserve WHERE the error occurred in explain-data.
+;So, we first call "make-check-item-locations", which creates new "s/def"s wrapping around the location list,
+;which is then used to validate each item.
+;So DO NOT call any of the :check-location/* specs directly, and instead call ::items.locations if you need to check.
 (defn make-check-item-locations [{:keys [locations]}]
   (s/def :check-location/location (into #{} locations))
   (s/def :check-location/item (s/keys :req-un [:check-location/location]))
