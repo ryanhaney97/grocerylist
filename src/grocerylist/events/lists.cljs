5228108fb92ec8d43c43f7f368bd1ab975018ed2
yoshiquest <yoshiquest@gmail.com>
2023-02-23 14:38:50 -0600

    Removed redundant events.

--- src/grocerylist/events/lists.cljs
+++ src/grocerylist/events/lists.cljs
@@ -21,12 +21,3 @@
 (reg-event-persistent-db
   ::delete
   delete)
-
-(defn confirm-delete [{db :db} [_ id]]
-  (let [current-list-name (get-in db [:lists id :name])]
-    {:fx [[::fx/confirm-dialog
-           {:message    (str "This will PERMANENTLY delete the list named " current-list-name ", are you sure you still want to delete it?")
-            :on-confirm #(re-frame/dispatch [::delete id])}]]}))
-(re-frame/reg-event-fx
-  ::confirm-delete
-  confirm-delete)
--- src/grocerylist/events/locations.cljs
+++ src/grocerylist/events/locations.cljs
@@ -27,20 +27,6 @@
   [select-list]
   delete)
 
-;(defn confirm-delete [{db :db} [_ itemnum]]
-;  (let [location (nth (:locations db) itemnum)
-;        location-counts (frequencies (map :location (vals (:items db))))
-;        num-references (get location-counts location 0)]
-;    (if (= num-references 0)
-;      {:fx [[:dispatch [::delete itemnum]]]}
-;      {:fx [[::fx/confirm-dialog
-;             {:message    (str "There are currently " num-references " items located in " location " that will be deleted as well. Do you still want to delete " location " ?")
-;              :on-confirm #(re-frame/dispatch [::delete itemnum])}]]})))
-;(re-frame/reg-event-fx
-;  ::confirm-delete
-;  [select-list]
-;  confirm-delete)
-
 (defn drag-start [db [_ itemnum]]
   (assoc db :location.dragged itemnum))
 (re-frame/reg-event-db
