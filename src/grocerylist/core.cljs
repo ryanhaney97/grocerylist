b05be88f3fabfb3288e08367712e93354c48d152
yoshiquest <yoshiquest@gmail.com>
2023-03-21 11:14:59 -0500

    Attempting to fix route not loading.

--- src/grocerylist/core.cljs
+++ src/grocerylist/core.cljs
@@ -2,6 +2,7 @@
   (:require
    [reagent.dom :as rdom]
    [re-frame.core :as re-frame]
+   [pushy.core :as pushy]
    [grocerylist.events :as events]
    [grocerylist.views :as views]
    [grocerylist.config :as config]
@@ -22,4 +23,6 @@
   (re-frame/dispatch-sync [::events/initialize-db])
   (routing/start!)
   (dev-setup)
-  (mount-root))
+  (mount-root)
+  (if (not (routing/parse (pushy/get-token routing/history)))
+    (re-frame/dispatch [:grocerylist.events.route/to :lists])))
